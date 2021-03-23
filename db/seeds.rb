b5c69653184545fa02d3a71dcedb60a2301307cf
Hariton <yourrasik@gmail.com>
2021-03-23 18:23:15 +1000

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create([{name: 'Iphone'}, {name: 'Ipad'}, {name: 'T-short'}, {name: 'Jeans'}, {name: 'Polo'}])
+Customer.create([{first_name: 'John', last_name: 'Doe', zipcode: '123123'}]) 
